7d1a2bdb3789a5c8ddbea13ab83024f0a6324321
lekondrateva <umpalus@gmail.com>
2022-06-12 16:05:04 +0500

    Задание №3.1 - Массив

--- src/main/java/Arrays.java
+++ src/main/java/Arrays.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Arrays {
@@ -5,14 +6,13 @@ public class Arrays {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите размер массива");
         int size = in.nextInt();
-        //считываем перевод строки
+        // считываем перевод строки
         in.nextLine();
         String[] myArray = new String[size];
         int maxLength = 0;
         int index = 0;
         for (int i = 0; i < size; i++) {
             System.out.println("Введите значение элемента массива №" + (i + 1));
-//            String buf = in.nextLine();
             myArray[i] = in.nextLine();
             if (myArray[i].length() > maxLength) {
                 maxLength = myArray[i].length();
@@ -20,5 +20,44 @@ public class Arrays {
             }
         }
         System.out.println("Самое длинное слово в массиве: " + myArray[index] + " элемент массива № " + (index + 1));
+    }
+
+    public static int[] fillArrayRandomly() {
+        int[] myArray = new int[20];
+        Random random = new Random();
+        System.out.println("Исходный массив: ");
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = random.nextInt(21) - 10;
+            System.out.print(myArray[i] + "  ");
+        }
+        return myArray;
+    }
+
+    public static void findMaxMinAndReplace(int[] array) {
+        int posMin = 10;
+        int negMax = -10;
+        int indexMax = 0;
+        int indexMin = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                if (array[i] > negMax) {
+                    negMax = array[i];
+                    indexMax = i;
+                }
+            } else if (array[i] > 0) {
+                if (array[i] < posMin) {
+                    posMin = array[i];
+                    indexMin = i;
+                }
+            }
+        }
+        System.out.println("\nМаксимальный отрицательный элемент массива = " + negMax + " с индексом: " + indexMax);
+        System.out.println("Минимальный положительный элемент массива = " + posMin + " с индексом: " + indexMin);
+        // перестановка значений
+        int buf = array[indexMax];
+        array[indexMax] = array[indexMin];
+        array[indexMin] = buf;
+        System.out.println("Массив после перестановки значений: ");
+        for (int i : array) System.out.print(i + "  ");
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 
 /**
  * @author Kondratyeva Lena
- * @see Calculator#calculate()
+ * @see Calculator#calculate() калькулятор с 4 операциями (+, -, *, /).
+ * @see Arrays#fillArrayRandomly() заполняет массив рандомными значениями от -10 до 10 и выводит его в консоль.
+ * @see Arrays#findMaxMinAndReplace(int[] array) находит минимальный положительный и минимальный отрицательный элементы массива,
+ * меняет их местами и выводит получившийся массив в консоль.
  */
 
 public class Main {
@@ -17,5 +20,7 @@ public class Main {
         } else if (task == 2) {
             Arrays.findTheLongestElement();
         } else System.out.println("Не удается распознать номер задачи, допустимы значения 1 или 2");
+            // Задание №3 - Массив
+        Arrays.findMaxMinAndReplace(Arrays.fillArrayRandomly());
     }
 }
